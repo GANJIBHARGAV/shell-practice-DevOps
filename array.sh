4dc4c0938ca70cc34d35aad20bd8d636e368921c
Bhargav Ganji <bhargavganji08.com>
2025-05-21 00:56:20 +0530

    Hello shell scripting started

--- array.sh
+++ array.sh
@@ -3,6 +3,6 @@
 movies=("babhubali" "billa" "darling")
 
 echo "First movie name in list is ${movies[0]}"
-echo "First movie name in list is ${movies[1]}"
-echo "First movie name in list is ${movies[2]}"
+echo "Second movie name in list is ${movies[1]}"
+echo "Third movie name in list is ${movies[2]}"
 echo "All movies are  ${movies[@]}"
