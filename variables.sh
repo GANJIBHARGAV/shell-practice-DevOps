0c01abf3b51054c2d4c7854f66cd59d062035bee
Bhargav Ganji <bhargavganji08.com>
2025-05-21 00:05:32 +0530

    Hello shell scripting started

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Name1="Bhargav";
-Name2="Rahul";
+Name1=Bhargav
+Name2=Rahul
 
 echo "$Name1 : How are you $Name2, when are you coming ?"
 echo "$Name2 : Hey $Name1, I am very busy in germany i cant come dude."
