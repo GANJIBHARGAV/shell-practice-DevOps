76157c87c94ffa6261aece38ebdbc8a00642c586
Bhargav Ganji <bhargavganji08.com>
2025-05-21 00:55:21 +0530

    Hello shell scripting started

---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+movies=("babhubali" "billa" "darling")
+
+echo "First movie name in list is ${movies[0]}"
+echo "First movie name in list is ${movies[1]}"
+echo "First movie name in list is ${movies[2]}"
+echo "All movies are  ${movies[@]}"
