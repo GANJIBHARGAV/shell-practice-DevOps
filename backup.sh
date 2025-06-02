49167fda73830bf5f869de42d8764f0cf07ac265
Bhargav Ganji <bhargavganji08.com>
2025-06-02 13:56:47 +0530

    shell-script-logs

---
+++ backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3: -14} #  If days it will take that day count otherwise default 14 days
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_FILE.log"
+
+if [ $USERID -ne 0 ]
+then 
+echo "Error: your not under the root user. Please check"
+else
+echo "Super you are under root user" | tee $LOG_FILE
+fi
+
+VALIDATE(){
+if [ $? eq 0 ]
+then 
+echo "$1 is sucess....." | tee $LOG_FILE
+else
+echo "$2 is failed....." | tee $LOG_FILE
+fi
+}
+
+USAGE(){
+    echo "USAGE:: sh backup.sh source_dir and dest_dir days(optional)"
+}
+
+if [ $# -lt 2 ]
+then 
+USAGE
+fi
+
+if[! -d $SOURCE_DIR ]
+then 
+echo "No source directory exists.Please check"
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+echo "No destination exists.Please check"
+fi
+
+
+
+
+
